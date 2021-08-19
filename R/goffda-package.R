8cfaea050b4b3f0e2ff50d5c29a2473149e3b27b
Eduardo García-Portugués <egarpor@users.noreply.github.com>
2021-08-19 02:35:27 +0200

    0.1.0

--- R/goffda-package.R
+++ R/goffda-package.R
@@ -7,19 +7,19 @@
 #' functional/scalar response and functional/scalar predictor. The package
 #' allows for the replication of the data applications considered in
 #' García-Portugués, Álvarez-Liébana, Álvarez-Pérez and González-Manteiga
-#' (2019) <arXiv:1909.07686>.
+#' (2021) <doi:10.1111/sjos.12486>.
 #'
 #' @author Eduardo García-Portugués and Javier Álvarez-Liébana.
 #' @references
 #' García-Portugués, E., Álvarez-Liébana, J., Álvarez-Pérez, G. and
-#' González-Manteiga, W. (2019). A goodness-of-fit test for the functional
-#' linear model with functional response. \emph{arXiv:1909.07686}.
-#' \url{https://arxiv.org/abs/1909.07686}
+#' Gonzalez-Manteiga, W. (2021). A goodness-of-fit test for the functional
+#' linear model with functional response. \emph{Scandinavian Journal of
+#' Statistics}, 48(2):502--528. \doi{10.1111/sjos.12486}
 #'
 #' García-Portugués, E., González-Manteiga, W. and Febrero-Bande, M. (2014). A
 #' goodness-of-fit test for the functional linear model with scalar response.
 #' \emph{Journal of Computational and Graphical Statistics}, 23(3):761--778.
-#' \url{http://doi.org/10.1080/10618600.2013.812519}
+#' \doi{10.1080/10618600.2013.812519}
 #' @docType package
 #' @name goffda-package
 #' @import graphics stats Rcpp
--- cran-comments.md
+++ cran-comments.md
@@ -1,13 +1,24 @@
-
-# Update 0.0.7
-
 ## Test environments
 
-* local OS X install, R 3.6.3
-* ubuntu 14.04 (on travis-ci), R 3.6.3
-* win-builder (devel and release)
+* local R installation, R 4.1.0
+* ubuntu 16.04 (on travis-ci), R 3.6.3
+* win-builder (release, devel)
+* Windows Server 2008 R2 SP1, R-devel, 32/64 bit (on R-hub)
+* Windows Server 2008 R2 SP1, R-release, 32/64 bit (on R-hub)
+* Windows Server 2008 R2 SP1, R-oldrel, 32/64 bit (on R-hub)
+* Ubuntu Linux 16.04 LTS, R-release, GCC (on R-hub)
+* Fedora Linux, R-devel, clang, gfortran (on R-hub)
+* Debian Linux, R-devel, GCC ASAN/UBSAN (on R-hub)
+* macOS 10.13.6 High Sierra, R-release, brew (on R-hub)
+* macOS 10.13.6 High Sierra, R-release, CRAN's setup (on R-hub)
+* Oracle Solaris 10, x86, 32 bit, R-release (on R-hub)
+* Oracle Solaris 10, x86, 32 bit, R-release, Oracle Developer Studio 12.6 (on R-hub)
 
-## Test results
+On some platforms:
 
-No errors. 
+* "Found the following (possibly) invalid URLs" -- double-checked and correctly spelled
+
+## R CMD check results
+
+0 errors | 0 warnings | 0 notes
 
